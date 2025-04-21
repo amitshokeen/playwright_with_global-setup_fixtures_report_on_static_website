9b1c6f756e2685e2e913ed13bb7003b685dac707
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2025-04-21 12:32:30 +1000

    username, pwd in secrets

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,13 +1,16 @@
 import { test as setup, expect } from '@playwright/test';
 import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const authFile = path.join(__dirname, '../testProject.outputDir/user_login.json');
 
 setup('authenticate', async ({ page }) => {
   // Perform authentication steps.
   await page.goto('https://narratives-fe.dev.ml-feapps.pulsarinternal.com/');
-  await page.getByPlaceholder('Enter your Username').fill('amit');
-  await page.getByPlaceholder('Enter your Password').fill('anoPwd@79999');
+  await page.getByPlaceholder('Enter your Username').fill(process.env.LOGIN_USERNAME!);
+  await page.getByPlaceholder('Enter your Password').fill(process.env.LOGIN_PASSWORD!);
   await page.getByRole('button', { name: 'Sign in', exact: true }).click();
     
   // Wait until the page receives the cookies.
